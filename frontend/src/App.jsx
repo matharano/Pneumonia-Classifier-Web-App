a2469b0bada0a7bcd3302353146ced3883be2f14
Mateus Harano <mon@certi.org.br>
2024-03-17 00:24:47 +0100

    Implemented connection to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,35 @@
 import './App.css';
+import React from 'react';
 import Home from './pages/home/home';
 
+
 function App() {
-  return (
-    <div className="App">
-        <Home />
-    </div>
-  );
+    const [image, setImage] = React.useState();
+
+    const handleImageSelection = (imageFile) => {
+        // Update image
+        setImage(imageFile)
+
+        // Send image to backend
+        let form = new FormData()
+        form.append('file', imageFile )
+
+        fetch("http://127.0.0.1:8000/predict", {
+            method: "POST",
+            mode: "cors",
+            body: form
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }
+
+    return (
+        <div className="App">
+            <Home
+                setImage={handleImageSelection}
+            />
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,4 +1,5 @@
 import './home.css';
+import React from 'react';
 import colors from '../../utils';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
@@ -12,22 +13,38 @@ const ColorButton = styled(Button)(({ theme }) => ({
     },
   }));
 
-function main() {
-  return (
-    <div className='Page'>
-        <header>
-            <h1>Pneumonia diagnosis</h1>
-            <p>Click in the button below to select an image of lungs xray or drop an image to infere whether it presents signs of pneumonia.</p>
-            <ColorButton
-                variant='outlined'
-                size='large'
-                endIcon={<UploadRoundedIcon />}
-            >
-                Upload image
-            </ColorButton>
-        </header>
-    </div>
-  );
+function Home({setImage}) {
+    const inputFileRef = React.useRef();
+
+    const handleImageSelection = (event) => {
+        if ( event.target.files && event.target.files[0] ) {
+            setImage(event.target.files[0]);
+        }
+    };
+
+    return (
+        <div className='Page'>
+            <header>
+                <h1>Pneumonia diagnosis</h1>
+                <p>Click in the button below to select an image of lungs xray or drop an image to infere whether it presents signs of pneumonia.</p>
+                <ColorButton
+                    variant='outlined'
+                    size='large'
+                    endIcon={<UploadRoundedIcon />}
+                    onClick={() => inputFileRef.current.click()}
+                >
+                    Upload image
+                </ColorButton>
+                <input
+                    className="Upload-button"
+                    type="file"
+                    accept=".jpg,.jpeg,.png"
+                    ref={inputFileRef}
+                    onChange={handleImageSelection}
+                    hidden />
+            </header>
+        </div>
+    );
 }
 
-export default main;
+export default Home;
